3ca235355006d04a5e97d98ece3e29b9ccce1afa
55839 <55839@10.0.0.106>
2021-03-01 12:01:20 -0300

    Segundo envio

--- RSA/src/Main.java
+++ RSA/src/Main.java
@@ -6,9 +6,8 @@ public class Main {
 		Scanner sc = new Scanner(System.in);
 
 		// passo 1: defina dois numeros primos
-		System.out.println("Digite os numeros primos p e q");
-		int p = sc.nextInt();
-		int q = sc.nextInt();
+		int p = 11;
+		int q = 17;
 
 		// passo 2: Calculo de n
 		int n = p * q;
@@ -17,26 +16,35 @@ public class Main {
 		int z = (p - 1) * (q - 1);
 
 		// passo 4: defina d
-		int d = 7;
+		int d = 7; // ((2 * z) + 1 ) / e;
+
 		// passo 5 valor de e
+
+		int contador = 0;
 		ArrayList<Integer> lista = new ArrayList<Integer>();
-		for (int ee = 1; ee < 1000; ee++) {
+		for (int ee = 2; contador < 1; ee++) {
 			if ((ee * d) % z == 1) {
 				lista.add(ee);
+				contador++;
 			}
 		}
 
 		int e = lista.get(0);
+		System.out.println(p + " " + q);
+		System.out.println("n = " + n);
+		System.out.println("z = " + z);
+		System.out.println("d = " + d);
+		System.out.println("e = " + lista);
 
 		// encriptar
-		System.out.println("Qual o valor da letra que você vai criptografar?");
+		System.out.println("Qual texto você vai criptografar?");
 		int textoplano = sc.nextInt();
 		int textocifrado = (int) Math.pow((textoplano), e);
 		int textocifradofinal = textocifrado % n;
-		System.out.println(textocifradofinal);
-		// desemcriptar
+		System.out.println("Texto encriptado: " + textocifradofinal);
+		// decriptar
 		int Textoplano = (int) Math.pow((textocifrado), d);
 		int Textoplanofinal = textoplano % n;
-		System.out.println(Textoplanofinal);
+		System.out.println("Texto descriptado: " + Textoplanofinal);
 	}
 }
